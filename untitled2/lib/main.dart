bd95e6a52fa6bb244b9c289dcb573c36a964227a
LAPTOP-N8E0I7QK\gotou <gotoukei57@gmail.com>
2022-11-28 08:19:23 +0900

    envファイル削除

--- untitled2/lib/main.dart
+++ untitled2/lib/main.dart
@@ -2,7 +2,4 @@ import 'package:flutter/material.dart';
 import 'package:untitled2/src/my_app.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-void main() async {
-  await dotenv.load(fileName: ".env");
-  runApp(const MyApp());
-}
+void main() => runApp(const MyApp());
